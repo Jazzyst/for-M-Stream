52e6064fcb7a46eb589f573fefdeaa5c716423fd
Jazzyst <andriy.zhuravlov@gmail.com>
2017-03-23 09:29:03 +0200

    markup

---
+++ js/common.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	$(".header-wrap nav ul a").mPageScroll2id();
+	$(".slide-text .btn a.button").mPageScroll2id();
+
+
+	
+});
+
+$(window).load(function() { 
+	$(".loader_inner").fadeOut(); 
+	$(".loader").delay(400).fadeOut("slow"); 
+});
